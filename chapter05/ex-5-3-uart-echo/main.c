01f1eb75c7e1ae61c577840dc4d16cbaa10ff2aa
Dhruv Mohindru <dmohindru@gmail.com>
2023-08-09 21:59:07 +1000

    Added code example for exercise 5-3, 5-4

---
+++ chapter05/ex-5-3-uart-echo/main.c
@@ -0,0 +1,33 @@
+#include "uart.h"
+#include <string.h>
+
+void Delay(uint32_t nTime);
+
+
+
+int main(void) {
+
+    uart_open(USART1, 9600, 0);
+
+    if (SysTick_Config(SystemCoreClock / 1000)) {
+        while (1);
+    }
+
+    while (1) {
+        int ch = uart_getc(USART1);
+        uart_putc(ch, USART1);
+    }
+}
+
+// Timer code
+static __IO uint32_t TimingDelay;
+
+void Delay(uint32_t nTime) {
+    TimingDelay = nTime;
+    while (TimingDelay != 0);
+}
+
+void SysTick_Handler(void) {
+    if (TimingDelay != 0x00)
+        TimingDelay--;
+}
---
+++ chapter05/ex-5-4-uart-echo-second/main.c
@@ -0,0 +1,36 @@
+#include "uart.h"
+#include <string.h>
+
+void Delay(uint32_t nTime);
+
+char string_to_send[] = "Hello from UART1\n\r";
+char second_string_to_send[] = "Hello from UART2\n\r";
+
+
+int main(void) {
+
+    uart_open(USART1, 9600, 0);
+    uart_open(USART2, 19200, 0);
+
+    if (SysTick_Config(SystemCoreClock / 1000)) {
+        while (1);
+    }
+
+    while (1) {
+        int ch = uart_getc(USART1);
+        uart_putc(ch, USART2);
+    }
+}
+
+// Timer code
+static __IO uint32_t TimingDelay;
+
+void Delay(uint32_t nTime) {
+    TimingDelay = nTime;
+    while (TimingDelay != 0);
+}
+
+void SysTick_Handler(void) {
+    if (TimingDelay != 0x00)
+        TimingDelay--;
+}
