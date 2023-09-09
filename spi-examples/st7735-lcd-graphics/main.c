e53b86682adc2b5f06594e318e77397cceae898b
Dhruv Mohindru <dmohindru@gmail.com>
2023-09-09 11:04:53 +1000

    Added rectangle drawing routine

--- spi-examples/st7735-lcd-graphics/main.c
+++ spi-examples/st7735-lcd-graphics/main.c
@@ -41,10 +41,8 @@ int main() {
 
     st7735_lcd_init(&st7735_Def);
     st7735_lcd_fill_screen(&st7735_Def, RED);
-
-//    char phrase[] = "Hello World!";
-    char phrase[] = "quick brown fox jumps over the lazy dog";
-    st7735_draw_str(&st7735_Def, phrase, WHITE, BLACK, 0, 0);
+    st7735_draw_rectangle(&st7735_Def, 10, 10, 50, 50, BLUE);
+    st7735_draw_rectangle(&st7735_Def, 60, 60, 90, 90, GREEN);
 
     while (1) {
         Delay(1000);
